fddb1af8acb3a3d4c8ca8836c27af8e506579613
bricemodeste <bricemodeste@Brices-MacBook-Pro.local>
2019-03-02 20:47:28 +0100

    sorting fixed and working, delete working almost well after some hours Alleluiiiaaaa

--- studentlist.js
+++ studentlist.js
@@ -8,9 +8,10 @@ const Poudlard_Student = {
   middleName: "-Unknown-",
   lastName: "-studentLastName-",
   imagename: "-studentImage-",
-  house: "-studentHouse-"
+  house: "-studentHouse-",
+  studentNombre: "-nb-"
 };
-const arrayOfStudents = [];
+let arrayOfStudents = [];
 let sortArray = [];
 let allFilter = document.querySelector("#filter_button_all");
 let slytherinFilter = document.querySelector("#filter_button_Slytherin");
@@ -20,6 +21,23 @@ let ravenclawFilter = document.querySelector("#filter_button_Ravenclaw");
 let sortByFirstNameSlt = document.querySelector("#sort_button_first");
 let sortByLastNameSlt = document.querySelector("#sort_button_last");
 
+let Countall = document.querySelector("button#filter_button_all span");
+let CountslytherinBtn = document.querySelector(
+  "button#filter_button_Slytherin span"
+);
+let CountgryffindorBtn = document.querySelector(
+  "button#filter_button_Gryffondor span"
+);
+let CounthufflepuffBtn = document.querySelector(
+  "button#filter_button_Hufflepuff  span"
+);
+let CountravenclawBtn = document.querySelector(
+  "button#filter_button_Ravenclaw span"
+);
+let expStudentCounter = document.querySelector("#expelled span");
+let Countstudent = 0;
+let expelledStudent = [];
+let expCount = 0;
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -45,6 +63,7 @@ function getJSON(studentList) {
       newStudent.lastName = fullName[1];
     }
     newStudent.house = showSingleStudent.house;
+    newStudent.studentNombre = Countstudent++;
     arrayOfStudents.push(newStudent);
   });
   displayStud(arrayOfStudents);
@@ -61,6 +80,39 @@ function displayStud(arraystud) {
 
     copy.querySelector("#data-lastName").textContent = nouveau.lastName;
     copy.querySelector("#data-house").textContent = nouveau.house;
+    let hogData = copy.querySelector("#design-data");
+
+    hogData.dataset.studentnb = nouveau.studentNombre;
+    let expBtn = copy.querySelector("#delete-button");
+    expBtn.dataset.expelnb = nouveau.studentNombre;
+    expBtn.addEventListener("click", function() {
+      // Remove
+
+      for (let i = arrayOfStudents.length - 1; i >= 0; --i) {
+        if (arrayOfStudents[i].studentNombre == nouveau.studentNombre) {
+          arrayOfStudents.splice(i, 1);
+
+          expelledStudent.push(nouveau);
+          Countstudent--;
+          expStudentCounter.textContent = expelledStudent.length;
+
+          CountslytherinBtn.textContent = filterHouse("Slytherin").length;
+          CountgryffindorBtn.textContent = filterHouse("Gryffindor").length;
+          CounthufflepuffBtn.textContent = filterHouse("Hufflepuff").length;
+          CountravenclawBtn.textContent = filterHouse("Ravenclaw").length;
+          Countall.textContent = arrayOfStudents.length;
+          console.log(arraystud);
+        }
+      }
+      for (let i = arraystud.length - 1; i >= 0; --i) {
+        if (arraystud[i].studentNombre == nouveau.studentNombre) {
+          arraystud.splice(i, 1);
+          console.log(arraystud);
+        }
+      }
+      hogData.style.display = "none";
+    });
+
     boxstudent.appendChild(copy);
     sortArray = arraystud;
   });
@@ -138,5 +190,3 @@ function sortByLastName() {
 
 loadSort();
 loadFilter();
-
-init();
